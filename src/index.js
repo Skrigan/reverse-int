6599d1ea4585be5167d2468829ccc9e17e0ab7b0
Skrigan Dmitry <sk_dm-821@mail.ru>
2022-07-09 00:37:11 +0300

    : )

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+
+	let nStr = n.toString(), res = '';
+
+	for (let i = nStr.length - 1; i >= 0; i--) {
+		if (nStr[i] == '-') {
+			return res;
+		}
+		res += nStr[i];
+	}
+
+	return res;
+
 }
